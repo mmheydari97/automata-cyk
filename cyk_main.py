ab4228997e6edd73eb8f82ef5647d3a11ed9e16f
mmheydari97 <heidarymm@yahoo.com>
2019-06-04 14:38:42 +0430

    first run

--- cyk_main.py
+++ cyk_main.py
@@ -1,9 +1,10 @@
 import os
-from builtins import print
 
 
 def create_cell(first, second):
     res = set()
+    if first == set() or second == set():
+        return set()
     for f in first:
         for s in second:
             res.add(f+s)
@@ -14,16 +15,19 @@ def read_grammar(filename="./grammar.txt"):
     filename = os.path.join(os.curdir, filename)
     with open(filename) as grammar:
         rules = grammar.readlines()
-        rights = []
-        lefts = []
+        variables = []
+        terminals = []
 
         for rule in rules:
             left, right = rule.split(" -> ")
             right = right[:-1].split(" | ")
-            lefts.append(left)
-            rights.append(right)
+            for r in right:
+                if str.islower(r):
+                    terminals.append([left, r])
+                else:
+                    variables.append([left, r])
 
-        return lefts, rights
+        return variables, terminals
 
 
 def read_input(filename="./input.txt"):
@@ -36,20 +40,32 @@ def read_input(filename="./input.txt"):
     return res
 
 
-def cyk_alg(V, T, inp):
+def cyk_alg(varies, terms, inp):
     length = len(inp)
-
+    var0 = [va[0] for va in varies]
+    var1 = [va[1] for va in varies]
     table = [[set() for _ in range(length-i)] for i in range(length)]
 
     # Deal with variables
     for i, cell in enumerate(table[0]):
-        cell.add(chr(65+i))
+
+        for te in terms:
+            if inp[i] == te[1]:
+                table[0][i].add(te[0])
 
     # Deal with terminals
+    for i in range(1, length):
+        for j in range(length - i):
+            for k in range(i):
+                row = create_cell(table[k][j], table[i-k-1][j+k+1])
+                for ro in row:
+                    if ro in var1:
+                        table[i][j].add(var0[var1.index(ro)])
+    for row in table:
+        print(row)
 
 
 if __name__ == '__main__':
-    read_grammar()
+    v, t = read_grammar()
     r = read_input()
-    print(r)
-    cyk_alg(None, None, "abbabb")
+    cyk_alg(v, t, "aaabbbcc")
