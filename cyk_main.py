42028ee70ce8e48acbff15023232aaf681c8c46b
mmheydari97 <heidarymm@yahoo.com>
2019-06-04 11:30:55 +0430

    read grammar

--- cyk_main.py
+++ cyk_main.py
@@ -6,15 +6,33 @@ def create_cell(first, second):
     return res
 
 
-length = 7
+def read_grammar(filename="./grammar.txt"):
+    with open(filename) as grammar:
+        rules = grammar.readlines()
+        rights = []
+        lefts = []
+
+        for rule in rules:
+            left, right = rule.split(" -> ")
+            right = right[:-1].split(" | ")
+            lefts.append(left)
+            rights.append(right)
+
+        return lefts, rights
 
 
-def cyk_alg():
+def cyk_alg(V, T, inp):
+    length = len(inp)
+
     table = [[set() for _ in range(length-i)] for i in range(length)]
+
+    # Deal with variables
     for i, cell in enumerate(table[0]):
         cell.add(chr(65+i))
-    print(table)
+
+    # Deal with terminals
+    read_grammar()
 
 
 if __name__ == '__main__':
-    cyk_alg()
+    cyk_alg(None, None, "abbabb")
