c5a43fc79c59e7ccb5807b8e582977af46953975
mmheydari97 <heidarymm@yahoo.com>
2019-06-04 10:30:49 +0430

    create table

--- cyk_main.py
+++ cyk_main.py
@@ -1,6 +1,20 @@
 def create_cell(first, second):
-    res = []
+    res = set()
     for f in first:
         for s in second:
-            res.append(f+s)
+            res.add(f+s)
+    return res
 
+
+length = 7
+
+
+def cyk_alg():
+    table = [[set() for _ in range(length-i)] for i in range(length)]
+    for i, cell in enumerate(table[0]):
+        cell.add(chr(65+i))
+    print(table)
+
+
+if __name__ == '__main__':
+    cyk_alg()
